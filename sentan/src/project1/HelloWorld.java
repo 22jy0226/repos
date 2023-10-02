a86b9327ced2074ad0f00e5ab15ecb0c6155b781
22jy0226 <22jy0226@PC19.jynet2.jec.ac.jp>
2023-10-02 14:11:21 +0900

    mod 乱数を10回から20回に増やして表示

--- sentan/src/project1/HelloWorld.java
+++ sentan/src/project1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
     	 Random random = new Random();
-    	 int randomNumber = random.nextInt(10) + 1;
+    	 int randomNumber = random.nextInt(20) + 1;
     	 for (int i = 0; i < randomNumber; i++) {
              System.out.println("HelloWorld");
          }
